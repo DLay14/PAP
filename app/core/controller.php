046f8db9afdb49665bd53aa403543c4b38732336
DLay14 <overjourney281@gmail.com>
2024-02-23 15:49:56 +0000

    comentarios

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,14 +1,17 @@
 <?php
 
+//Criação da class controller
 Class Controller
 {
-
+    //função view que carrega um if para verificar a existencia de um ficheiro
     public function view($path, $data = [])
-    {
+    {  
+        //se o ficheiro existir inclui na pagina inicial
         if(file_exists("../app/views/admin/" . $path . ".php"))
         {
             include "../app/views/admin/" . $path . ".php";
         }
+        //se nao, é mostrado a pagina de erro 404 
         else
         {
             $this->view("404");
--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
-
+// Inicia a sessão
 session_start();
 
+// Inclui o arquivo de inicialização
 include "../app/init.php";
 
+// Cria uma instância da classe App
 $app = new App();
-
 ?>
