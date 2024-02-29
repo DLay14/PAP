fad08f0b5985c5215df26097bedc280c65fe267d
DLay14 <overjourney281@gmail.com>
2024-02-29 15:27:45 +0000

    Tutorial 5

--- app/views/admin/_includes/admin_header.php
+++ app/views/admin/_includes/admin_header.php
@@ -9,7 +9,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link href="<?= ASSETS . THEME ?>img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,12 +21,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="<?= ASSETS . THEME ?>lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?= ASSETS . THEME ?>lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= ASSETS . THEME ?>css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.0/js/bootstrap.min.js" rel="stylesheet"> -->
+
+    
 
     <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="<?= ASSETS . THEME ?>css/style.css" rel="stylesheet">
+
+    <?php var_dump(ASSETS . THEME . "css/bootstrap.min.css")?>
 </head>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,17 @@
 // Inicia a sessão
 session_start();
 
+//print_r($_SERVER);
+
+$path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+$path = str_replace("index.php", "", $path);
+
+define('ROOT', $path);
+
+define('ASSETS', $path . "assets/");
+
+define('THEME', 'admin/');
+
 // Inclui o arquivo de inicialização
 include "../app/init.php";
 
